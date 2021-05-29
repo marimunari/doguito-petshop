600916b9af4322993f788961d0c40557ab4a6fa4
marimunari <54755896+marimunari@users.noreply.github.com>
2021-05-29 19:49:27 -0300

    Cadastro de Clientes

---
+++ controller/cadastraClientes-controller.js
@@ -0,0 +1,18 @@
+import { clienteService } from '../service/cliente-service.js';
+
+const form = document.querySelector('[data-form]');
+
+form.addEventListener('submit', async (evento) => {
+    evento.preventDefault();
+
+    try {
+        const nome = evento.target.querySelector('[data-nome]').value;
+        const email = evento.target.querySelector('[data-email]').value;
+
+        await clienteService.criaCliente(nome, email)
+        window.location.href = '../telas/cadastro_concluido.html';
+    } catch(erro) {
+        console.log(erro);
+        window.location.href = '../telas/erro.html';
+    }
+});
